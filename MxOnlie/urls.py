7382fd011d12ff6f7825c62796a6331d11ac64a9
MjSeven <yuchenmj@163.com>
2018-07-22 15:56:34 +0800

    首页和登录页面的配置初始化完成

--- MxOnlie/urls.py
+++ MxOnlie/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.views.generic import TemplateView
 
 import xadmin
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
+    path('', TemplateView.as_view(template_name="index.html"), name='index'),
+    path('login/', TemplateView.as_view(template_name="login.html"), name="login"),
 ]
